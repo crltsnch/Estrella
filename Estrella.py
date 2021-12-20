f00f0d634c37c27d1bc3a57f3c4396e00e333fcd
crltsnch <crltsnch@myuax.com>
2021-12-20 19:17:59 +0100

    Código cuando el cursor tiene que pintar

--- Estrella.py
+++ Estrella.py
@@ -5,11 +5,12 @@ turtle.shape("classic")
 def estrella(turtle, n, d=100):
     angulo=360/n
     if n%2==0:
-        puntas=[]
+        lineas=[]
         for a in range(0, n):
             turtle.penup()
-            puntas.append(turtle.pos())
+            lineas.append(turtle.pos())
             turtle.circle(d, angulo)
-    
-
-        return angulo
+        for b in range(0, len(lineas)):
+            if b%2==0:
+                turtle.pendown()
+                turtle.goto(lineas[b][0], lineas[b],[1])
